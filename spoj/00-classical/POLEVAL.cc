a47b599e96319dfc5df7ec0c8b32abe6ae81e9c9
kwachmat <kwasniak.mateusz@gmail.com>
2015-08-31 15:21:58 +0200

    spoj/00-classical/POLEVAL.cc

---
+++ spoj/00-classical/POLEVAL.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long horner(int n, int c[], int x)
+{
+    long long res = c[0];
+    for(int i=0; i<n; i++) res = res*x + c[i+1];
+    
+    return res;
+}
+
+int main()
+{
+    unsigned k, cases = 0;
+    int n, x, coe[1001];
+    
+    ios_base::sync_with_stdio(0);
+    
+    // polynomial
+    while(cin >> n)
+    {
+        if(n == -1) break;
+        
+        cout << "Case " << ++cases << ":\n";
+        for(int i=0; i<=n; i++) cin >> coe[i];
+        
+        // points
+        cin >> k;
+        while(k--)
+        {
+            cin >> x;
+            cout << horner(n, coe, x) << "\n";
+        }
+    }
+    
+    return 0;
+}
