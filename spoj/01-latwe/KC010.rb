ba53e8bf3b2ea9f54d302c5a200dbe84a74d7fda
kwachmat <kwasniak.mateusz@gmail.com>
2015-09-03 09:58:23 +0200

    spoj/01-latwe/KC010.rb

--- spoj/01-latwe/KC010.rb
+++ spoj/01-latwe/KC010.rb
@@ -1,4 +1,4 @@
-while input = $stdin.gets do
+while input = gets do
     
     words_count = numbers_count = 0
     input.split(' ').each { |i| (i =~ /[0-9]/ ? numbers_count += 1 : words_count += 1) }
