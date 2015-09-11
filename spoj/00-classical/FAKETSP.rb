43729c4b87f36edd33c395c8eea818e6eeb6df7e
kwachmat <kwasniak.mateusz@gmail.com>
2015-09-11 20:12:07 +0200

    spoj/00-classical/FAKETSP.rb

--- spoj/00-classical/FAKETSP.rb
+++ spoj/00-classical/FAKETSP.rb
@@ -2,10 +2,10 @@ distsum = 0.00
 prev_x, prev_y = gets[0..-3].split('(').last.split(', ').map(&:to_f)
 
 while input = gets do
-	x, y = input[0..-3].split('(').last.split(', ').map(&:to_f)
+    x, y = input[0..-3].split('(').last.split(', ').map(&:to_f)
 
-	distsum += Math.sqrt((x-prev_x)**2 + (y-prev_y)**2)
-	printf("The salesman has traveled a total of %.3f kilometers.\n", distsum)
+    distsum += Math.sqrt((x-prev_x)**2 + (y-prev_y)**2)
+    printf("The salesman has traveled a total of %.3f kilometers.\n", distsum)
 	
-	prev_x, prev_y = x, y
+    prev_x, prev_y = x, y
 end
