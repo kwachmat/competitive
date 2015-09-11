69b1b3de70bec26ef0107fb8665a10d75cc439fa
kwachmat <kwasniak.mateusz@gmail.com>
2015-09-11 18:32:30 +0200

    spoj/00-classical/FAKETSP.cc

---
+++ spoj/00-classical/FAKETSP.cc
@@ -0,0 +1,11 @@
+distsum = 0.00
+prev_x, prev_y = gets[0..-3].split('(').last.split(', ').map(&:to_f)
+
+while input = gets do
+	x, y = input[0..-3].split('(').last.split(', ').map(&:to_f)
+
+	distsum += Math.sqrt((x-prev_x)**2 + (y-prev_y)**2)
+	printf("The salesman has traveled a total of %.3f kilometers.\n", distsum)
+	
+	prev_x, prev_y = x, y
+end
