8cd5b3c5a232a6d2d2ab890054bae15a6d45d0ca
Mateusz Kwaśniak <kwasniak.mateusz@gmail.com>
2015-09-15 17:17:01 +0200

    spoj/02-srednie/NOE.rb

--- spoj/02-srednie/NOE.rb
+++ spoj/02-srednie/NOE.rb
@@ -1,9 +1,2 @@
-# CODE GOLF VERSION:
+# CODE GOLF, COMPRESSED VERSION:
 p @x if $<.map { |num| @x = @x.to_i ^ num.to_i }
-
-# UNCOMPRESSED VERSION:
-
-#while num = gets
-#   x = x.to_i ^ num.to_i
-#end
-#puts x
