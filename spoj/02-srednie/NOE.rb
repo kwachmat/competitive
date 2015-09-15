ad4d9dff03a39b926650710e764402a31ad012a1
Mateusz Kwaśniak <kwasniak.mateusz@gmail.com>
2015-09-15 17:16:10 +0200

    spoj/02-srednie/NOE.rb

--- spoj/02-srednie/NOE.rb
+++ spoj/02-srednie/NOE.rb
@@ -3,9 +3,7 @@ p @x if $<.map { |num| @x = @x.to_i ^ num.to_i }
 
 # UNCOMPRESSED VERSION:
 
-=begin
-while num = gets
-  x = x.to_i ^ num.to_i
-end
-puts x
-=end
+#while num = gets
+#   x = x.to_i ^ num.to_i
+#end
+#puts x
