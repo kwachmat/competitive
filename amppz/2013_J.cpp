c3eb3c204c0c79092f1fcb587215a475dd60a5b9
kwachmat <kwasniak.mateusz@gmail.com>
2015-10-08 16:19:04 +0200

    amppz/2013_J.cpp

--- amppz/2013_J.cpp
+++ amppz/2013_J.cpp
@@ -2,16 +2,16 @@
 
 int main() {
     
-    int n, i;
+    long long n, i;
     long long res = 0;
     
-    scanf("%d", &n);
-    for(i=2; 2*i<=n; i<<=1)
+    scanf("%lld", &n);
+    for(i=2; 2*i<=n; i*=2)
     {
-        res += i*(i-1)>>1;
+        res += i*(i-1)/2;
     }
     
-    res += (n-i)*(n+1-i)>>1;
+    res += (n-i)*(n+1-i)/2;
     printf("%lld\n", res);
     
     return 0;
