0e0e0e3539aca82e2b5f16ea0769299bb0ca90a2
kwachmat <kwasniak.mateusz@gmail.com>
2015-10-29 17:51:11 +0100

    amppz/2015_F.cpp

---
+++ amppz/2015_F.cpp
@@ -0,0 +1,37 @@
+#include <stack>
+#include <string>
+#include <iostream>
+using namespace std;
+
+inline char getOpening(char c) { return (c == ')' ? '(' : (c == ']' ? '[' : '{')); }
+
+int main() {
+    
+    stack<char> S;
+    string str, Q = "";
+    ios_base::sync_with_stdio(0);
+    
+    cin >> str;
+    
+    for(char c : str) 
+    {
+        if(c == ')' or c == ']' or c == '}')
+        {
+            if(S.empty()) Q = getOpening(c) + Q;
+            else if(S.top() == getOpening(c)) S.pop();
+            else {
+                cout << "NIE\n";
+                return 0;
+            }
+        }
+        else S.push(c);
+    }
+    
+    // out
+    for(char c : Q) cout << c;
+    cout << str;
+    while(!S.empty()) { cout << getClosing(S.top()); S.pop(); }
+    
+    cout << "\n";
+    return 0;
+}   
