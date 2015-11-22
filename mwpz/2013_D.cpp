36012e260f00175d9c37ea73f949eec9300c1871
kwachmat <kwasniak.mateusz@gmail.com>
2015-11-22 20:34:05 +0100

    MWPZ 2013: Task D

--- mwpz/2013_D.cpp
+++ mwpz/2013_D.cpp
@@ -1,9 +1,9 @@
 int main() {
     
     int x = 5;
-    for(int rok=2014; rok<2114; rok++, x--)
+    for(int year=2014; year<2114; year++, x--)
     {
-        if(rok%4==0 && rok!=2100) x--;
+        if(year%4==0 && year!=2100) x--;
         
         if(x <= 0) x+= 7;
         printf("%d %d\n", x, x+1);
