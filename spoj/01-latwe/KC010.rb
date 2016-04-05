d2895f7ac1fc64efc6a014dd083b6e42d313ea85
kwachmat <kwasniak.mateusz@gmail.com>
2016-04-05 19:01:05 +0200

    Removed some easy solutions

--- spoj/01-latwe/KC010.rb
+++
@@ -1,7 +0,0 @@
-while input = gets do
-    
-    words_count = numbers_count = 0
-    input.split(' ').each { |i| (i =~ /[0-9]/ ? numbers_count += 1 : words_count += 1) }
-
-    print numbers_count, ' ', words_count, "\n"
-end
